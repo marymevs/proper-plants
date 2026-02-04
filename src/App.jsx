ab291a2cd6ec68c7714549630693a59adec418fa
Mary Imevbore <mimevbore14@gmail.com>
2026-02-04 15:36:31 -0500

    Format the cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import Plants from "./Plants";
 import Cart from "./Cart";
 export default function App() {
+  // const [cart, setCart] = useState();
   return (
     <>
       <header>
         <h1>Proper Plants</h1>
       </header>
       <main>
-        <h2>Plants</h2>
-        <Plants></Plants>
-        <h2>Cart</h2>
-        <Cart></Cart>
+        <section>
+          <h2>Plants</h2>
+          <Plants></Plants>
+        </section>
+        <section id="cart-section">
+          <h2>Cart</h2>
+          <Cart></Cart>
+        </section>
       </main>
     </>
   );
--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,17 +1,43 @@
-const cart = [
-  {
-    id: 1,
-    image: "🌿",
-    name: "Fern",
-  },
-];
-
 export default function Cart() {
+  const cart = [
+    {
+      id: 1,
+      image: "🌿",
+      name: "Fern",
+    },
+    {
+      id: 2,
+      image: "🌵",
+      name: "Cactus",
+    },
+    {
+      id: 3,
+      image: "🌻",
+      name: "Sunflower",
+    },
+  ];
   return (
-    <ul>
+    <ul className="cart">
       {cart.map((cartItem) => (
-        <li>{cartItem.name}</li>
+        <li>
+          <p>
+            {cartItem.image} {cartItem.name}
+          </p>
+          <p>
+            <span>
+              <button>-</button>
+              <p>1</p>
+              <button>+</button>
+            </span>
+          </p>
+        </li>
       ))}
     </ul>
   );
+
+  function addToCart(plant) {
+    const newCart = cart;
+    newCart.push(plant);
+    setCart(newCart);
+  }
 }
